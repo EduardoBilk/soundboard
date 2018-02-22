737efc4ce297df0f26efa13fd846d4648a10a96c
Eduardo Bilk <eduardobilk@gmail.com>
2018-02-21 22:43:50 -0300

    relative path for deploy

--- controllers/soundbordController.js
+++ controllers/soundbordController.js
@@ -6,7 +6,7 @@ const uuid = require('uuid');
 const multerOptions = {
   storage: multer.diskStorage({
     destination: function(req, file, next) {
-      next(null, './uploads');
+      next(null, 'public/uploads');
     },
     filename: function(req, file, next) {
       const extension = file.mimetype.split('/')[1];
